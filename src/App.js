dca11a2ae0facd7b7f6457e3346efde950ade0cd
hvtk <hvtk1968@gmail.com>
2021-04-02 10:51:06 +0200

    edded error searchBar

--- src/App.js
+++ src/App.js
@@ -12,14 +12,17 @@ const apiKey = 'a7cd75638653e0e76e1cf755f173d483';
 function App() {
   const [weatherData, setWeatherData] = useState(null);
   const [location, setLocation] = useState('');
+  const [error, setError] = useState(false);
 
 useEffect(() => {
   async function fetchData() {
+    setError(false);
     try {
       const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location},nl&appid=${apiKey}&lang=nl`);
       setWeatherData(result.data);
     } catch (e) {
       console.error(e);
+      setError(true);
     }
   }
     if (location) {
@@ -33,6 +36,11 @@ useEffect(() => {
         {/*HEADER -------------------- */}
         <div className="weather-header">
           <SearchBar setLocationHandler={setLocation}/>
+          {error && (
+              <span className="wrong-location-error">
+                Oeps! deze locatie bestaat niet
+              </span>
+          )}
 
           <span className="location-details">
             {weatherData &&
