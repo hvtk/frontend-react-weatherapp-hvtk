652626217fdc1952f0846ff4699987d3f24ca663
hvtk <hvtk1968@gmail.com>
2021-04-02 12:51:39 +0200

    final code till 8.1

--- src/pages/forecastTab/ForecastTab.js
+++ src/pages/forecastTab/ForecastTab.js
@@ -9,49 +9,61 @@ function ForecastTab({coordinates}) {
   const [forecasts, setForecasts] = useState(null);
   const [error, setError] = useState(false);
 
+
   function createDateString(timestamp) {
     const day = new Date(timestamp * 1000);
-    return day.toLocaleDateString(`nl-NL`, { weekday: 'long'});
+    return day.toLocaleDateString('nl-NL', {weekday: 'long'});
   }
 
   useEffect(() => {
     async function fetchData() {
+      setError(false);
+
       try {
-        const result = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates?.lat}&lon=${coordinates?.lon}&exclude=minutely,current,hourly&appid=${apiKey}&lang=nl\`);
+        const result = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates?.lat}&lon=${coordinates?.lon}&exclude=minutely,current,hourly&appid=${apiKey}&lang=nl`);
         setForecasts(result.data.daily.slice(1, 6));
       } catch (e) {
         console.error(e);
+        setError(true);
       }
-    }  
-        
+    }
+
     if (coordinates) {
       fetchData();
     }
-      
+
   }, [coordinates]);
 
   return (
-    <div className="tab-wrapper">
-      {forecasts && forecasts.map((day) => {
-        return (
-          <article className="forecast-day" key={forecast.dt}>
-            <p className="day-description">
-             {createDateString(forecast.dt)}
-            </p>
+      <div className="tab-wrapper">
+        {forecasts && forecasts.map((forecast) => {
+          return (
+              <article className="forecast-day" key={forecast.dt}>
+                <p className="day-description">
+                  {createDateString(forecast.dt)}
+                </p>
 
-            <section className="forecast-weather">
+                <section className="forecast-weather">
               <span>
                 {forecast.temp.day}
               </span>
-              <span className="weather-description">
+                  <span className="weather-description">
                 {forecast.weather[0].description}
               </span>
-            </section>
-          </article>
-        )
-      })}
-    </div>
+                </section>
+              </article>
+          )
+        })}
+
+        {!forecasts && !error && (
+            <span className="no-forecast">
+          Zoek eerst een locatie om het weer voor deze week te bekijken
+        </span>
+        )}
+
+        {error && <span> Er is iets misgegaan met het ophalen van de data. </span>}
+      </div>
   );
-);  
+}
 
 export default ForecastTab;
